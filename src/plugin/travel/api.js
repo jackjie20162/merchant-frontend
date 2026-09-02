20583c3f36014cabf6045b589833b93879b5dad3
jackjie20162 <610841147@qq.com>
2026-09-02 19:33:08 +0800

    feat(travel): expose payment API facade

--- src/plugin/travel/api.js
+++ src/plugin/travel/api.js
@@ -1,7 +1,9 @@
-import { listTravelProducts, getTravelProduct, checkTravelInventory, createTravelOrder, getTravelOrder } from '../../api/travel'
+import { listTravelProducts, getTravelProduct, checkTravelInventory, createTravelOrder, getTravelOrder, createTravelPayment, getTravelPayment } from '../../api/travel'
 
 export const travelProductList = listTravelProducts
 export const travelProductDetail = getTravelProduct
 export const travelInventoryCheck = checkTravelInventory
 export const travelOrderCreate = createTravelOrder
 export const travelOrderDetail = getTravelOrder
+export const travelPaymentCreate = createTravelPayment
+export const travelPaymentDetail = getTravelPayment
